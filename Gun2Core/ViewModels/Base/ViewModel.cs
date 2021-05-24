ff5e4616b1c33e9f886542ad3ea0531a906849b5
kpblc2000@gmail.com <kpblc2000@gmail.com>
2021-05-24 20:35:05 +0300

    Базовая ViewModel

---
+++ Gun2Core/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gun2Core.ViewModels.Base
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        public virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value))
+            {
+                return false;
+            }
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
+        public virtual string Title
+        {
+            get { return _Title; }
+            set { Set(ref _Title, value); }
+        }
+
+        private string _Title;
+
+    }
+}
