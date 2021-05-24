b026f9d25fc65771f514953e87e7ec28428a4868
kpblc2000@gmail.com <kpblc2000@gmail.com>
2021-05-24 20:25:53 +0300

    Модель пользователя и преставление пользователей (пока пустое)

---
+++ Gun2Core/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gun2Core.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string MidName { get; set; }
+        public string LastName { get; set; }
+
+        public string Department { get; set; }
+
+        public bool IsDeveloper { get; set; }
+    }
+}
---
+++ Gun2Core/Views/Windows/UserWindowView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gun2Core.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для UserWindowView.xaml
+    /// </summary>
+    public partial class UserWindowView : Window
+    {
+        public UserWindowView()
+        {
+            InitializeComponent();
+        }
+    }
+}
