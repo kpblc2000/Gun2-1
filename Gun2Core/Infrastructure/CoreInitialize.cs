3ce7a8b0aa4cda229c910fa86b49e93e939f5a73
kpblc2000@gmail.com <kpblc2000@gmail.com>
2021-05-26 16:43:11 +0300

    Начальные настройки приложения. Идеи по поводу выполнения настроек

---
+++ Gun2Core/Infrastructure/CoreInitialize.cs
@@ -0,0 +1,44 @@
+﻿using Gun2Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gun2Core.Infrastructure
+{
+    public class CoreInitialize
+    {
+        public static void Initialize()
+        {
+            CoreSettings settings = new CoreSettings();
+            if (string.IsNullOrEmpty( settings.DbConnectionString)
+                || string.IsNullOrEmpty(settings.SettingsFileName)
+                || !File.Exists(settings.SettingsFileName)
+                )
+            {
+                // ToDo Принудительную установку снять
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                
+                builder.DataSource = Environment.GetEnvironmentVariable("computername").ToString();
+                builder.InitialCatalog = "Gun2CAD";
+                                builder.ConnectTimeout = 180;
+                builder.IntegratedSecurity = true;
+                builder.PersistSecurityInfo = false;
+
+                settings.DbConnectionString = builder.ConnectionString;
+
+                settings.Save();
+
+                if (string.IsNullOrEmpty(settings.SettingsFileName)
+                || !File.Exists(settings.SettingsFileName))
+                {
+
+                }
+
+            }
+        }
+    }
+}
---
+++ Gun2Core/Models/CoreSettings.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gun2Core.Models
+{
+    internal class CoreSettings
+    {
+        public CoreSettings()
+        {
+            RegistryKey curUser = Registry.CurrentUser;
+            RegistryKey _BaseKey = curUser.CreateSubKey(_ParentRegistryHive);
+            DbConnectionString = _BaseKey.GetValue(_DbConnectionStringKey) as string;
+            SettingsFileName = _BaseKey.GetValue(_SettingsFileName) as string;
+            _BaseKey.Close();
+        }
+
+        public void Save()
+        {
+            _BaseKey.SetValue(_DbConnectionStringKey, DbConnectionString);
+            _BaseKey.SetValue(_SettingsFileName, SettingsFileName);
+            _BaseKey.Close();
+        }
+
+        public string DbConnectionString { get; set; }
+        public string SettingsFileName { get; set; }
+
+        private readonly string _ParentRegistryHive = @"SOFTWARE\kpblc2000";
+        private readonly string _DbConnectionStringKey = "DbConnectionString";
+        private readonly string _SettingsFileName = "SettingsFileName";
+        private RegistryKey _BaseKey;
+        
+    }
+}
