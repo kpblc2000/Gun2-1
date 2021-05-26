b6e0df8f33f84bdb851056df52956a05b33c4ee8
kpblc2000@gmail.com <kpblc2000@gmail.com>
2021-05-26 17:30:22 +0300

    ViewModel и View для CoreSettings

---
+++ Gun2Core/ViewModels/CoreSettingsViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Gun2Core.Models;
+using Gun2Core.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gun2Core.ViewModels
+{
+    internal class CoreSettingsViewModel : ViewModel
+    {
+        public CoreSettingsViewModel()
+        {
+            Title = "Настройки приложения";
+            _CoreSettings = new CoreSettings();
+            DbConnectionString = _CoreSettings.DbConnectionString;
+            SettingsFileName = _CoreSettings.SettingsFileName;
+        }
+
+        #region Публичные свойства
+        public string DbConnectionString
+        {
+            get { return _DbConnectionString; }
+            set { Set(ref _DbConnectionString, value); }
+        }
+
+        public string SettingsFileName
+        {
+            get { return _SettingsFileName; }
+            set { Set(ref _SettingsFileName, value); }
+        } 
+        #endregion
+
+        private CoreSettings _CoreSettings;
+        private string _DbConnectionString;
+        private string _SettingsFileName;
+    }
+}
